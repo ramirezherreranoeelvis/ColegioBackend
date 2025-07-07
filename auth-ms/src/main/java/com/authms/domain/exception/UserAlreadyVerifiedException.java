e4bc19ed40ebf900e95255fdc05287b877cde817
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-07-06 22:57:44 -0500

    .

--- auth-ms/src/main/java/com/authms/domain/exception/UserAlreadyVerifiedException.java
+++
@@ -1,7 +0,0 @@
-package com.authms.domain.exception;
-
-public class UserAlreadyVerifiedException extends RuntimeException {
-    public UserAlreadyVerifiedException(String message) {
-        super(message);
-    }
-}
